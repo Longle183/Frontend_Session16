579722ca5699ab4ec28b5b65438ce4ab2a1d98d0
Longle183 <lethanhlong1224@gmail.com>
2025-02-26 11:05:20 +0700

    Add files via upload

--- btvn session16 10.js
+++ btvn session16 10.js
@@ -1,4 +1,8 @@
-let inputString = prompt("Nhập vào một chuỗi bất kỳ:");
-let words = inputString.split(" ");
-let wordCount = words.length;
-alert("Số lượng từ trong chuỗi là: " + wordCount);
+let input = prompt("Nhập vào 2 số bất kỳ, cách nhau bằng dấu phẩy (ví dụ: 3,9):");
+let numbers = input.split(",");
+let num1 = parseInt(numbers[0]);
+let num2 = parseInt(numbers[1]);
+let min = Math.min(num1, num2);
+let max = Math.max(num1, num2);
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min;
+alert("Số ngẫu nhiên trong khoảng là: " + randomNum);
